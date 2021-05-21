ee9053df2cc720076b7264eec1bfd2d63dbfa56d
Thays Silva <thaysconceicaods@gmail.com>
2021-05-21 00:00:12 -0300

    Create routes

---
+++ legendary_app/lib/Home.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class HomePageView extends StatefulWidget {
+
+  @override
+  _HomePageViewState createState() => _HomePageViewState();
+}
+
+class _HomePageViewState extends State<HomePageView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Home"),
+        ),
+        body: Center(
+          child: Text("Página modelo para Rotas")
+        )
+    );
+  }
+}
---
+++ legendary_app/lib/RouteGenerator.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:legendary_app/Home.dart';
+
+class RouteGenerator {
+
+  /// Adicionar aqui uma constante com o nome da sua rota, como no exemplo abaixo
+  /// E adicionar um novo CASE no switch mais abaixo
+  static const String ROTA_HOME = "/home";
+
+  /// E quando for mudar de branch, seguir o modelo abaixo
+  // Navigator.pushReplacementNamed(context, RouteGenerator.ROTA_HOME)
+
+  static Route<dynamic> generateRoute(RouteSettings settings) {
+
+    switch(settings.name){
+      case ROTA_HOME:
+        return MaterialPageRoute(
+            builder: (_) => HomePageView()
+        );
+      default:
+        return MaterialPageRoute(
+            builder: (_) => HomePageView()
+        );
+    }
+
+  }
+
+}
