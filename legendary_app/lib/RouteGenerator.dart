52642bc4ee0a4cb200b6e88b6997786d7df6f05e
Marília Costa <marilia.gouveia.costa@gmail.com>
2021-06-01 20:03:09 -0300

    Route

--- legendary_app/lib/RouteGenerator.dart
+++ legendary_app/lib/RouteGenerator.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:legendary_app/CarregarImagem.dart';
 import 'package:legendary_app/Home.dart';
 
 class RouteGenerator {
   /// Adicionar aqui uma constante com o nome da sua rota, como no exemplo abaixo
   /// E adicionar um novo CASE no switch mais abaixo
   static const String ROTA_HOME = "/home";
+  static const String ROTA_CADASTRARIMAGEM = "/carregarimagem";
 
   /// E quando for mudar de branch, seguir o modelo abaixo
   // Navigator.pushReplacementNamed(context, RouteGenerator.ROTA_HOME)
@@ -13,6 +15,10 @@ class RouteGenerator {
     switch (settings.name) {
       case ROTA_HOME:
         return MaterialPageRoute(builder: (_) => HomePageView());
+
+      case ROTA_CADASTRARIMAGEM:
+        return MaterialPageRoute(builder: (_) => UploadImagePage());
+
       default:
         return MaterialPageRoute(builder: (_) => HomePageView());
     }
