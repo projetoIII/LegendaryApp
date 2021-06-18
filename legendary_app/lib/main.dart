96432cff2ab93c8cea722481e805dbe77749cb10
Marília Costa <marilia.gouveia.costa@gmail.com>
2021-06-18 00:11:14 -0300

    Rotas - Ajuste

--- legendary_app/lib/main.dart
+++ legendary_app/lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:legendary_app/Home.dart';
 
-import 'CarregarImagem.dart';
 import 'RouteGenerator.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(MaterialApp(
-      home: UploadImagePage(),
+      home: HomePageView(),
       theme: ThemeData(
           primaryColor: Colors.purple,
           appBarTheme: AppBarTheme(color: Color(0xFFCE93D8))),
