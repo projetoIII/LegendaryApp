d807b3dbf76438f31f7f0f06e69230f81f73da31
Marília Costa <marilia.gouveia.costa@gmail.com>
2021-06-19 14:57:43 -0300

    Tela de Favoritos - Funcionalidade: Testes Firebase (Correção)

--- legendary_app/lib/widgets/LegendaCard.dart
+++ legendary_app/lib/widgets/LegendaCard.dart
@@ -1,20 +1,34 @@
 import 'package:flutter/material.dart';
 
 class LegendaCard extends StatelessWidget {
+  late String id;
+  late String legenda;
+  late String titulo;
+  late bool categoria;
+  late Function(bool) onFavorite;
+  late bool favorito;
 
-  final String legenda;
-  final String titulo;
-  final bool categoria;
-  final Function(bool) onFavorite;
-  final bool favorito;
-
-  LegendaCard({
-      this.legenda = "",
+  LegendaCard(
+      {this.legenda = "",
       this.titulo = "",
       this.categoria = true,
       this.favorito = false,
-      required this.onFavorite
-  });
+      required this.onFavorite});
+
+  LegendaCard.fromJson(Map<String, dynamic> json) {
+    if (json == null) return;
+    id = json['id'];
+    titulo = json['titulo'];
+    legenda = json['legenda'];
+    favorito = json['favorito'];
+  }
+
+  Map<String, dynamic> toJson() => {
+        'id': id,
+        'titulo': titulo,
+        'legenda': legenda,
+        'favorito': favorito,
+      };
 
   @override
   Widget build(BuildContext context) {
@@ -28,22 +42,21 @@ class LegendaCard extends StatelessWidget {
             width: MediaQuery.of(context).size.width * 0.2,
             height: 70,
             child: Card(
-              color: Color(0xFFEBBAF3),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: this.categoria
-                  ? Icon(
-                    Icons.music_note,
-                    size: 30,
-                    color: Colors.white,
-                  )
-                  : Icon(
-                Icons.menu_book_rounded,
-                size: 30,
-                color: Colors.white,
-              )
-            ),
+                color: Color(0xFFEBBAF3),
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                child: this.categoria
+                    ? Icon(
+                        Icons.music_note,
+                        size: 30,
+                        color: Colors.white,
+                      )
+                    : Icon(
+                        Icons.menu_book_rounded,
+                        size: 30,
+                        color: Colors.white,
+                      )),
           ),
           SizedBox(
             width: MediaQuery.of(context).size.width * 0.7,
@@ -62,22 +75,15 @@ class LegendaCard extends StatelessWidget {
                           child: Column(
                             crossAxisAlignment: CrossAxisAlignment.center,
                             children: <Widget>[
-                              Text(
-                                  this.legenda,
-                                  style: TextStyle(
-                                      color: Colors.black
-                                  )),
+                              Text(this.legenda,
+                                  style: TextStyle(color: Colors.black)),
                               Padding(padding: EdgeInsets.only(top: 10)),
-                              Text(
-                                  this.titulo,
+                              Text(this.titulo,
                                   style: TextStyle(
                                       color: Colors.black,
-                                      fontWeight: FontWeight.bold
-                                  )),
+                                      fontWeight: FontWeight.bold)),
                             ],
-                          )
-                      )
-                  ),
+                          ))),
                   IconButton(
                       icon: this.favorito
                           ? Icon(Icons.favorite, size: 30)
